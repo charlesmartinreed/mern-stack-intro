5b8f0437a08f64eea324653d084032a4e6efbc07
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-11 17:31:06 -0500

    Restart project.

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
-const express = require('express');
-
-// interact directly with mongoDB
-const mongoose = require('mongoose');
-
-// take requests, get data from body when, for instance, making a POST request
-const bodyParser = require('body-parser');
-
-// api routing
-const items = require('./routes/api/items');
-
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-
-app.use(bodyParser.json());
-
-// DB Config
-const db = require('./config/keys').mongoURI;
-
-// Connect to MongoDB
-mongoose
-	.connect(db)
-	.then(() => console.log('MongoDB Connected...'))
-	.catch(err => console.log(err));
-
-// anythign that hits api/items should utilize the
-app.use('/api/items', items);
-
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server started up on port ${port}`));
+
+// MIDDLEWARE
+app.use(express.json());
+
+// DATABASE
+const db = require("./config/keys").mongoURI;
+
+// Connect to mLab DB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.listen(port, () => console.log(`Server started on ${port}!`));
