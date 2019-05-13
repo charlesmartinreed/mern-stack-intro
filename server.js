8fd90040a6a4ea8e2885f0341d6038168f0f030c
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-13 16:35:32 -0500

    Version 1.0.0

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require("path");
 
 const items = require("./routes/api/items");
 
@@ -10,6 +11,17 @@ app.use(express.json());
 
 // Use routes
 app.use("/api/items", items);
+
+// Serve static assets IF IN PRODUCTION
+if (process.env.NODE_ENV === "production") {
+  // set static folder to load the index.html in client/build
+  app.use(express.static("client/build"));
+
+  //any request that isn't api/items should hit this route
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // DATABASE
 const db = require("./config/keys").mongoURI;
