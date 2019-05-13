dab163d5acad4f2669e3cfc019edff3e28c9e41a
charlesmartinreed <charlesmartinreed@icloud.com>
2019-05-13 15:51:02 -0500

    Refactor getItems action to pull data from api/items.

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,9 +1,16 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "./types";
+import axios from "axios";
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from "./types";
 
-export const getItems = () => {
-  return {
-    type: GET_ITEMS
-  };
+// beause we've added our proxy in package.json, we don't need to type the full local URL
+// no need to convert to JSON since it's sent to us from the api as JSON
+export const getItems = () => dispatch => {
+  dispatch(setItemsLoading());
+  axios.get("/api/items").then(res =>
+    dispatch({
+      type: GET_ITEMS,
+      payload: res.data
+    })
+  );
 };
 
 export const deleteItem = id => {
@@ -17,5 +24,12 @@ export const addItem = item => {
   return {
     type: ADD_ITEM,
     payload: item
+  };
+};
+
+// long story short, this sets loading from its initial state of 'false' to 'true'
+export const setItemsLoading = () => {
+  return {
+    type: ITEMS_LOADING
   };
 };
--- client/src/reducers/itemReducers.js
+++ client/src/reducers/itemReducers.js
@@ -1,22 +1,24 @@
 import uuid from "uuid";
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  DELETE_ITEM,
+  ITEMS_LOADING
+} from "../actions/types";
 
-// STATIC DATA FOR TESTING
 const initialState = {
-  items: [
-    { id: uuid.v4(), name: "Eggs" },
-    { id: uuid.v4(), name: "Seasoning Salt" },
-    { id: uuid.v4(), name: "Cereal" },
-    { id: uuid.v4(), name: "Oranges" }
-  ]
+  items: [],
+  loading: false
 };
 
-// items: [action.payload, ...state.items]
+// once we get the payload, set the loading to false again
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
       return {
-        ...state
+        ...state,
+        items: action.payload,
+        loading: false
       };
     case DELETE_ITEM:
       return {
@@ -27,6 +29,11 @@ export default function(state = initialState, action) {
       return {
         ...state,
         items: state.items.concat([action.payload])
+      };
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true
       };
     default:
       return state;
